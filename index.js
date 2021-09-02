8444dd71364ff6a079440f4526cbdf0c1827fc9b
Gasparini Osvaldo <gaspariniosvaldo1@gmail.com>
2021-09-02 17:08:42 -0300

    Rework

--- index.js
+++ index.js
@@ -1,68 +1,51 @@
-/* Inicio de sesion */
+/* Toma de datos del Local Storage para mostrar nombre de usuario */
 
 function inicioSesion() {
-    let value;
-    value = localStorage.getItem("nombreUsuario");
-    if (value != null) {
-        msgBienvenida = document.getElementById("bienvenida");
-        msgBienvenida.innerHTML = "Bienvenid@ " + localStorage.getItem("nombreUsuario");
+    let nombre = localStorage.getItem("nombreUsuario");
+    let msgBienvenida = $("#bienvenida");
+
+    if (nombre != null) {
+        msgBienvenida.html(`<a href="signIn.html">Bienvenid@ ${localStorage.getItem("nombreUsuario")} </a>`);
     } else {
-        msgBienvenida = document.getElementById("bienvenida");
-        msgBienvenida.innerHTML = `<a href="signIn.html">Inicia sesion</a>`
+        msgBienvenida.html(`<a href="signIn.html">Inicia sesion</a>`);
     }
 }
+
 inicioSesion();
 
+/* Contador */
 
-/* Counter */
-
-const counter = document.getElementById("counter");
-const inc = document.getElementById("inc");
-const dec = document.getElementById("dec");
+let counter = $("#counter");
+let inc = $("#inc");
+let dec = $("#dec");
 
 let count = 0;
 
-inc.addEventListener("click", () => {
+inc.click(() => {
     count++;
-    counter.value = count;
-})
+    counter.val(count);
+});
 
-dec.addEventListener("click", () => {
-    if (counter.value > 0) {
+dec.click(() => {
+    if (counter.val() > 0) {
         count--;
-        counter.value = count;
+        counter.val(count);
     }
-})
+});
 
-/* Generar HTML */
-class Zapatilla {
-    constructor(id, marca, mod, precio) {
-        this.id = id;
-        this.marca = marca;
-        this.mod = mod;
-        this.precio = precio;
-    }
+
+/* Activar carrito de compras */
+
+function detectarCompra() {
+    $(".activarCarrito").click(() => {
+        if ($('.activarCarrito').is(':checked')) {
+            $("#msgCarritoVacio").hide();
+            $(".bxs-cart").show();
+        } else {
+            $(".bxs-cart").hide();
+            $("#msgCarritoVacio").show();
+        }
+    })
 }
 
-const zapa1 = new Zapatilla(1, "Nike", "Pegasus 38", 3000);
-const zapa2 = new Zapatilla(2, "Adidas", "React", 2450);
-const zapa3 = new Zapatilla(3, "Puma", "Joyride", 3100);
-const zapa4 = new Zapatilla(4, "Nike", "Jordan", 5000)
-
-const listaZapatillas = [zapa1, zapa2, zapa3, zapa4];
-
-function mostrarListaDeZapatillas() {
-    for (const zapa of listaZapatillas) {
-        let contenedor = document.createElement("div");
-
-        contenedor.innerHTML = `<h3> Marca: ${zapa.marca}<h3>
-        <p> Modelo: ${zapa.mod}<p>
-        <p> Precio: $ ${zapa.precio}<p>
-        <h6> ---------------------------------- <h6>`;
-
-        document.body.appendChild(contenedor);
-    }
-}
-
-const button = document.getElementById("btnShowSneakers");
-button.addEventListener("click", mostrarListaDeZapatillas);
+detectarCompra();
--- iniciarSesion.js
+++ iniciarSesion.js
@@ -1,17 +1,33 @@
 let nombre;
 
-nombre = document.getElementById("nombre");
-
-function volverHome(){
+function volverHome() {
     location.href = "index.html";
 }
 
-function registrarUsuario(){
-    if(nombre) {
-        localStorage.setItem("nombreUsuario", nombre.value);
-        volverHome();
-    }
+function registrarUsuario() {
+    nombre = $("#nombre").val();
+
+    if (nombre) {
+        localStorage.setItem("nombreUsuario", nombre);
+
+        const URLGET = "https://jsonplaceholder.typicode.com/posts"
+
+        const infoPost = {
+            nombre: nombre
+        };
+        $.post(URLGET, infoPost, (respuesta, estado) => {
+            if (estado === "success") {
+                $(".iniciarSesion__form").slideUp("slow", "swing");
+                $("#anuncioIniciasteSesion").html(`<h2>Listo! Ya estas registrado ${nombre} </h2>`);
+                $("#anuncioIniciasteSesion").show("swing");
+            }
+        });
+    };
+
+    setTimeout(volverHome, 5000);
 }
 
-let btnRegistrarse = document.getElementById("registrarse");
-btnRegistrarse.addEventListener("click", registrarUsuario);
+let btnRegistrarse = $("#registrarse");
+btnRegistrarse.click(registrarUsuario);
+
+/* animate({"right": "70vw"}, 2000) */
